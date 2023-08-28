abc7e5c446bdfc6c0fd6a43ef09805143640764b
wineandchord <guoqizhou19@gmail.com>
2023-08-28 11:30:00 +0800

    add copyright

--- errors.go
+++ errors.go
@@ -1,4 +1,10 @@
-// Package jaeger trpc-opentracing-jaeger 插件
+// Tencent is pleased to support the open source community by making tRPC available.
+// Copyright (C) 2023 THL A29 Limited, a Tencent company. All rights reserved.
+// If you have downloaded a copy of the tRPC source code from Tencent,
+// please note that tRPC source code is licensed under the Apache 2.0 License,
+// A copy of the Apache 2.0 License is included in this file.
+
+// Package jaeger is the trpc-opentracing-jaeger plugin.
 package jaeger
 
 import (
